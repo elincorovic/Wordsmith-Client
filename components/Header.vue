c1f690e5d1659b94fa18225e50fd35cc1801461a
elincorovic <ec024@hdm-stuttgart.de>
2023-10-02 04:23:52 +0200

    initial commit

---
+++ components/Header.vue
@@ -0,0 +1,63 @@
+<script></script>
+
+<template>
+    <div id="container">
+        <header>
+            <div id="logo-container">
+                <img src="~/assets/logo.png" id="burger-icon" />
+            </div>
+            <div id="burger-icon-container">
+                <img src="~/assets/icons/menu-burger.png" id="burger-icon" />
+            </div>
+        </header>
+
+        <div id="overlay">
+            <a class="overlay-link">Home</a>
+            <a class="overlay-link">Books</a>
+            <a class="overlay-link">About Us</a>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+header {
+    height: 8vh;
+    background-color: $accent;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-left: 15px;
+    padding-right: 15px;
+}
+
+h1 {
+    margin: 0;
+    color: white;
+}
+
+#overlay {
+    height: 92vh;
+    position: absolute;
+    background-color: $accent;
+    padding-left: 15px;
+    padding-right: 15px;
+    color: white;
+    width: 100vw;
+}
+
+#logo-container {
+    width: 200px;
+}
+
+#logo {
+    width: 100%;
+}
+
+#burger-icon-container {
+    width: 33px;
+}
+
+#burger-icon {
+    width: 100%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Home</h1>
+    </div>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
+        excepturi, ratione numquam commodi, rerum eligendi ad harum alias itaque
+        aliquam laborum eius cumque. Nostrum tempora veritatis labore adipisci
+        perferendis harum debitis, atque obcaecati quae beatae est molestias
+        iusto maiores consequuntur minus quis repellendus? Reprehenderit unde
+        accusamus quisquam cum! Ducimus neque laudantium atque praesentium
+        ratione placeat eveniet dolor porro esse.
+    </p>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
+        excepturi, ratione numquam commodi, rerum eligendi ad harum alias itaque
+        aliquam laborum eius cumque. Nostrum tempora veritatis labore adipisci
+        perferendis harum debitis, atque obcaecati quae beatae est molestias
+        iusto maiores consequuntur minus quis repellendus? Reprehenderit unde
+        accusamus quisquam cum! Ducimus neque laudantium atque praesentium
+        ratione placeat eveniet dolor porro esse.
+    </p>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit
+        excepturi, ratione numquam commodi, rerum eligendi ad harum alias itaque
+        aliquam laborum eius cumque. Nostrum tempora veritatis labore adipisci
+        perferendis harum debitis, atque obcaecati quae beatae est molestias
+        iusto maiores consequuntur minus quis repellendus? Reprehenderit unde
+        accusamus quisquam cum! Ducimus neque laudantium atque praesentium
+        ratione placeat eveniet dolor porro esse.
+    </p>
+</template>
